a88bf990968474c83d1d38de314437120cf332ba
Blake Smith <blakesmith0@gmail.com>
2014-04-15 16:59:39 -0500

    Remove deprecated http method

--- lib/tempodb/client.rb
+++ lib/tempodb/client.rb
@@ -223,44 +223,14 @@ module TempoDB
       response
     end
 
-    def do_http_deprecated(uri, request) # :nodoc:
-      if @http_client.nil?
-        @http_client = HTTPClient.new
-        @http_client.transparent_gzip_decompression = true
-        if @secure
-          @http_client.ssl_config.clear_cert_store
-          @http_client.ssl_config.set_trust_ca(TempoDB::TRUSTED_CERT_FILE)
-        end
-      end
-
-      request.basic_auth @key, @secret
-      request['User-Agent'] = "tempodb-ruby/#{TempoDB::VERSION}"
-      request['Accept-Encoding'] = "gzip"
-
-      method = request.method.downcase.intern
-      http_client_attrs = {
-        :header => Hash[request.to_hash.map {|k,v| [k, v.first] if v.is_a?(Array) && v.size == 1}],
-        :body => request.body
-      }
-
-      begin
-        response = @http_client.request(method, uri, http_client_attrs)
-      rescue OpenSSL::SSL::SSLError => e
-        raise TempoDBClientError.new("SSL error connecting to TempoDB.  " +
-                                     "There may be a problem with the set of certificates in \"#{TempoDB::TRUSTED_CERT_FILE}\".  " + e)
-      end
-
-      parse_response(response)
+    def do_get(url, params = nil, headers = nil)  # :nodoc:
+      uri = build_uri(url, params)
+      parse_response(do_http(uri, Net::HTTP::Get.new(uri.request_uri, headers)))
     end
 
-    def do_get(url, params=nil, headers=nil)  # :nodoc:
+    def do_delete(url, params = nil, headers = nil)
       uri = build_uri(url, params)
-      do_http_deprecated(uri, Net::HTTP::Get.new(uri.request_uri, headers))
-    end
-
-    def do_delete(url, params=nil, headers=nil)
-      uri = build_uri(url, params)
-      do_http_deprecated(uri, Net::HTTP::Delete.new(uri.request_uri, headers))
+      parse_response(do_http(uri, Net::HTTP::Delete.new(uri.request_uri, headers)))
     end
 
     def do_http_with_body(uri, request, body)
@@ -276,15 +246,15 @@ module TempoDB
           request.body = s
         end
       end
-      do_http_deprecated(uri, request)
+      parse_response(do_http(uri, request))
     end
 
-    def do_post(url_parts, headers=nil, body=nil)  # :nodoc:
+    def do_post(url_parts, headers = nil, body = nil)  # :nodoc:
       uri = build_uri(url_parts)
       do_http_with_body(uri, Net::HTTP::Post.new(uri.request_uri, headers), body)
     end
 
-    def do_put(url_parts, headers=nil, body=nil)  # :nodoc:
+    def do_put(url_parts, headers = nil, body = nil)  # :nodoc:
       uri = build_uri(url_parts)
       do_http_with_body(uri, Net::HTTP::Put.new(uri.request_uri, headers), body)
     end
@@ -307,7 +277,7 @@ module TempoDB
       if response.ok?
         body = response.body
 
-        if body == ""
+        if body.empty?
           return {}
         else
           return JSON.parse(body)
